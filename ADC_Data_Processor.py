58c483a468577dddc5e4bd5418c82c013ef3ec8f
Jack-J-Day <jack.day@stfc.ac.uk>
2021-10-21 15:31:31 +0100

    Update ADC_Data_Processor.py
    Re-ord of main calls and layout

--- ADC_Data_Processor.py
+++ ADC_Data_Processor.py
@@ -12,10 +12,16 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
+# set times to process between
+#start_time = datetime.datetime(year=2021, month=10, day=15, hour=10, minute=46)   # Data Collection start time
+start_time = datetime.datetime(year=2021, month=10, day=20, hour=12, minute=30)   # Data Collection start time
+#end_time = datetime.datetime(year=2021, month=10, day=18, hour=16, minute=1)
+end_time = datetime.datetime.now()
+
 HEADER_STRING = "ffffffffffffffff0"
 END_HEADER = "efffffffffffffff0"
 
-def Packet_Preprocessing(Packet_Data):  # Split each packet into its frames
+def PacketFrameSplitter(Packet_Data):  # Split each packet into its frames
     Processed_Packet = Packet_Data.replace(HEADER_STRING,
                                            ":" + HEADER_STRING)  # replace all occurances of the frame header string in the packet with ":", this will tell us where to split the packet later on
     Processed_Packet = Processed_Packet.replace(END_HEADER,
@@ -25,45 +31,22 @@ def Packet_Preprocessing(Packet_Data):  # Split each packet into its frames
     return Processed_Packets
 
 def HeaderProcessor(Packet_ToProcess):  # extracts header data and returns the output
-
     PacketHeader = Packet_ToProcess[0:128]  # Extract the first 128 Chars from packet - this is the header part
     binary_header = bin(int(PacketHeader, base=16))[2:]  # Convert the Hex Header into binary for data extraction, remove the first two chars to remove "0b" infront of string
 
-    Years_STR = binary_header[128:136] # Extract binary from header for FrameTime - Years
-    FrameTime_Years = int(Years_STR, 2) # Convert binary extract into int
-
-    Days_STR = binary_header[136:145]  # Extract binary from header for FrameTime - Days
-    FrameTime_Days = int(Days_STR, 2)  # Convert binary extract into int
-
-    Hours_STR = binary_header[145:150]  # Extract binary from header for FrameTime - Hours
-    FrameTime_Hours = int(Hours_STR, 2)  # Convert binary extract into int
-
-    Mins_STR = binary_header[150:156]  # Extract binary from header for FrameTime - Mins
-    FrameTime_Mins = int(Mins_STR, 2)  # Convert binary extract into int
-
-    Secs_STR = binary_header[156:162]  # Extract binary from header for FrameTime - Secs
-    FrameTime_Secs = int(Secs_STR, 2)  # Convert binary extract into int
-
-    mS_STR = binary_header[162:172]  # Extract binary from header for FrameTime - mS
-    FrameTime_mS = int(mS_STR, 2)  # Convert binary extract into int
-
-    uS_STR = binary_header[172:182]  # Extract binary from header for FrameTime - uS
-    FrameTime_uS = int(uS_STR, 2)  # Convert binary extract into int
-
-    nS_STR = binary_header[182:192]  # Extract binary from header for FrameTime - nS
-    FrameTime_nS = int(nS_STR, 2)
-    # print("FrameTime Info: ",FrameTime_Years, ":",FrameTime_Days,":",FrameTime_Hours,":",FrameTime_Mins,":",FrameTime_Secs,":",FrameTime_mS,":",FrameTime_uS,":",FrameTime_nS)
+    FrameTime_Years = int(binary_header[128:136], 2)    # Extract year's (as int) from header binary
+    FrameTime_Days = int(binary_header[136:145], 2)     # Extract Day's (as int) from header binary
+    FrameTime_Hours = int(binary_header[145:150], 2)    # Extract Hour's (as int) from header binary
+    FrameTime_Mins = int(binary_header[150:156], 2)     # Extract Min's (as int) from header binary
+    FrameTime_Secs = int(binary_header[156:162], 2)     # Extract Sec's (as int) from header binary
+    FrameTime_mS = int(binary_header[162:172], 2)       # Extract mS's (as int) from header binary
+    FrameTime_uS = int(binary_header[172:182], 2)       # Extract uS's (as int) from header binary
+    FrameTime_nS = int(binary_header[182:192], 2)       # Extract nS's (as int) from header binary
 
     # Convert streamed date into nS Since Epoch
-    FrameTime = (365 * 8.64e+13) * FrameTime_Years + 30  # add nS since epoch to this year - streamed out is years past 2000 so add 30 for past epoch
-    FrameTime += (FrameTime_Days * 8.64e+13)
-    FrameTime += (FrameTime_Hours * 3.6e+12)
-    FrameTime += (FrameTime_Mins * 6e+10)
-    FrameTime += (FrameTime_Secs * 1e+9)
-    FrameTime += (FrameTime_mS * 1000000)
-    FrameTime += (FrameTime_uS * 1000)
-    FrameTime += FrameTime_nS
-    FrameTime = int(FrameTime)
+    FrameTime = int(((365 * 8.64e+13) * FrameTime_Years + 30) + (FrameTime_Days * 8.64e+13) +
+                    (FrameTime_Hours * 3.6e+12) + (FrameTime_Mins * 6e+10) + (FrameTime_Secs * 1e+9) +
+                    (FrameTime_mS * 1000000) + (FrameTime_uS * 1000) + FrameTime_nS)
 
     PeriodNo_STR = binary_header[208:224]
     PeriodNumber = int(PeriodNo_STR, 2)
@@ -74,12 +57,13 @@ def HeaderProcessor(Packet_ToProcess):  # extracts header data and returns the o
     EventsInFrame_STR = binary_header[225:256]
     EventsInFrame = int(EventsInFrame_STR, 2)
 
-    # print("Frame No: ",FrameNumber," Events in Frame: ", EventsInFrame, " Frame Time: ", FrameTime," Period No:", PeriodNumber)
-
     return FrameNumber, EventsInFrame, FrameTime, PeriodNumber, binary_header
 
-def PacketProcessor_MAPS(Packet_Data, numerror , numprocessedevents, SRC_IP):  # Pulls the event data out of the streamed packets
+def PacketProcessor_MAPS(Packet_Data, SRC_IP):  # Pulls the event data out of the streamed packets
     # Note this function only gets MAPs Data from the event packet - Time from TOF, Position and pulse height
+
+    numprocessedevents = 0
+    numerror = 0
 
     Frame_Posisitions = []
     Frame_PulHs = []
@@ -87,23 +71,18 @@ def PacketProcessor_MAPS(Packet_Data, numerror , numprocessedevents, SRC_IP):  #
     Frame_PulHOverFlows =[]
     Frame_Times = []
 
-    CH_MantidDectID = []
-    CH_MantidDectLen = []
     IPWiringTable = df_WiringTable.loc[(df_WiringTable['StreamingIP'] == SRC_IP)]
-
-    for Channel in range(6):
-        CH_MantidDectID.append(IPWiringTable['Mantid_DetectorID_Start'].iloc[Channel])
-        CH_MantidDectLen.append(IPWiringTable['Mantid_Detector_ID_Lenght'].iloc[Channel])
+    CH_MantidDectID = IPWiringTable['Mantid_DetectorID_Start'].tolist()
+    CH_MantidDectLen = IPWiringTable['Mantid_Detector_ID_Lenght'].tolist()
 
     numEvents = int((len(Packet_Data) / 16)-1) # work out the number of events in the packet by dividing 16 (event lenght) - and subtracting one for loop
     for event in range(0, numEvents):       # For each event in the packet
         EventStartADD = (event * 16)        # calculate the start address of the event data
         EventEndADD = EventStartADD + 16    # calculate the end address of the event data
 
-        hexEvent = Packet_Data[EventStartADD:EventEndADD]   # Get the hex data from packet between the start/end of event
-        binEvent = bin(int(hexEvent, base=16))[2:]          # convert hex into binary - remove first two chars as they are always 0b
-        if hexEvent[0:2] == "e0" and len(binEvent) == 64:   # if packet starts with e0 and has correct len
-            binEvent = bin(int(hexEvent, base=16))[2:]
+        # hexEvent =    # Get the hex data from packet between the start/end of event
+        binEvent = bin(int(Packet_Data[EventStartADD:EventEndADD], base=16))[2:]          # convert hex into binary - remove first two chars as they are always 0b
+        if len(binEvent) == 64:   # if packet starts with e0 and has correct len
 
             PulHOverflow = binEvent[38:39]
             PosOverflow = binEvent[39:40]
@@ -126,20 +105,13 @@ def PacketProcessor_MAPS(Packet_Data, numerror , numprocessedevents, SRC_IP):  #
             numprocessedevents += 1
         else:
             numerror += 1
-            print("INVALID EVENT DETECTED, Error: ", numerror, " Bad Event: ", hexEvent, " Error SRC IP: ", SRC_IP)
+            print("INVALID EVENT DETECTED, Bad Event: ", Packet_Data[EventStartADD:EventEndADD], " Error SRC IP: ", SRC_IP)
 
-  #   print("Frame Times List: ", Frame_Times)
-  #   print("Event Positions: ", Frame_Posisitions)
-  #   print("Frame PulH: ", Frame_PulHs)
-  #   print("Frame Position Overflows: ", Frame_PosOverFlows)
-  #   print("Frame PulH Overflows: ", Frame_PulHOverFlows)
     return Frame_Times, Frame_Posisitions, Frame_PulHs, Frame_PulHOverFlows, Frame_PosOverFlows, numerror, numprocessedevents
-
 
 def WiringTableReader(CSV_File_Location):  # Reads in the CSV containing wiring table information
     WiringTablePD = pd.read_csv(CSV_File_Location)
     return WiringTablePD
-
 
 def Serialise_EV42_ISIS_Data(period_number, run_state, proton_charge, source_name, message_id, pulse_time,
                              time_of_flight, detector_id):
@@ -176,10 +148,7 @@ def Serialise_EV42(source_name, message_id, pulse_time, time_of_flight, detector
     return EV42
 
 
-#start_time = datetime.datetime(year=2021, month=10, day=15, hour=10, minute=46)   # Data Collection start time
-start_time = datetime.datetime(year=2021, month=10, day=20, hour=12, minute=30)   # Data Collection start time
-#end_time = datetime.datetime(year=2021, month=10, day=18, hour=16, minute=1)
-end_time = datetime.datetime.now()
+
 print("Data Start Time: ", start_time, " Data End Time: ", end_time)
 
 # Collect Kafka Data
@@ -192,61 +161,58 @@ collect_time = datetime.datetime.now() - collect_start          # Calc total col
 # Read in instrument DAES wiring table
 df_WiringTable = WiringTableReader('DAES_WiringTable_MAPS.csv')
 
-numerror = 0            # Couter for number of events that reported errors
-numprocessedevents = 0  # Counter for number of processed events
-TotalPacketCount = 0    # Counter for total number of packets
+totalnumerror = 0            # Couter for number of events that reported errors
+totalnumprocessedevents = 0  # Counter for number of processed events
+TotalFrameCount = 0    # Counter for total number of packets
 
 loopstart = datetime.datetime.now() # Get time of processing start
 
 #Process Historic Kafka Data - run for each packet in the kafka data
-for packet in range(0, len(all_data)):
-    PacketFrames = Packet_Preprocessing(all_data[packet].get("packet")) # split the packet into list of frames
-    TotalPacketCount += len(PacketFrames)                                    # Add number of frame to running total
-    CurrentFrameSRC = str(all_data[packet].get("packet_info"))          # Get Current Packets source IP
+with cProfile.Profile() as pr:
+    for packet in range(0, len(all_data)):
+        PacketFrames = PacketFrameSplitter(all_data[packet].get("packet")) # split the packet into list of frames
+        TotalFrameCount += len(PacketFrames)                                    # Add number of frame to running total
+        CurrentFrameSRC = str(all_data[packet].get("packet_info"))          # Get Current Packets source IP
 
-    # for each of the frames in the current packet:
-    for f in range(0, len(PacketFrames)):
-        HeaderData = HeaderProcessor(PacketFrames[f])               # process header data - returns list
-        EventData = PacketFrames[f][128:len(PacketFrames[f])]       # Define event data (framedata - header)
+        # for each of the frames in the current packet:
+        for f in range(0, len(PacketFrames)):
+            HeaderData = HeaderProcessor(PacketFrames[f])               # process header data - returns list
+            EventData = PacketFrames[f][128:len(PacketFrames[f])]       # Define event data (framedata - header)
 
-        # process the frame into events
-        result = PacketProcessor_MAPS(EventData, numerror, numprocessedevents,
-                                      all_data[packet].get("packet_info"))
+            # process the frame into events
 
-        # push the data into ESS Flatbuffer format
-        EV42_FrameData = Serialise_EV42(CurrentFrameSRC, numprocessedevents, HeaderData[2], result[0], result[1])
+            result = PacketProcessor_MAPS(EventData, all_data[packet].get("packet_info"))
 
-        #
-        numerror = result[5]                    # get packet processor number of errors
-        numprocessedevents = int(result[6])     # get packet processor number of events
+            # push the data into ESS Flatbuffer format
+            EV42_FrameData = Serialise_EV42(CurrentFrameSRC, HeaderData[0], HeaderData[2], result[0], result[1])
+            totalnumerror += result[5]                    # get packet processor number of errors
+            totalnumprocessedevents += int(result[6])     # get packet processor number of events
 
 currenttime = datetime.datetime.now()           #get processing complete time
 overallrun = currenttime - collect_start        #Calculate overall runtime
 processingtime = currenttime - loopstart        #calculate runtime
 
-print("ADC Data Processor Runtime Statistics: - header --> Packet")
+print("ADC Data Processor Runtime Statistics:")
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print("Kafka Collection Time: ", collect_time)
 print("")
 print("Started process: ", loopstart, ", Processing finished: ", currenttime)
 print("Processing Time: ", processingtime)
 print("")
-print("Total Frame Processing Rate: ", TotalPacketCount / processingtime.total_seconds(), " (Frames Per Second)")
-print("Total Event Processing Rate: ", numprocessedevents / processingtime.total_seconds(), " (Events Per Second)")
+print("Total Frames: ", TotalFrameCount)
+print("Total Events: ", totalnumprocessedevents)
+print("Total Event Errors: ", totalnumerror)
+print("")
+print("Total Frame Processing Rate: ", TotalFrameCount / processingtime.total_seconds(), " (Frames Per Second)")
+print("Total Event Processing Rate: ", totalnumprocessedevents / processingtime.total_seconds(), " (Events Per Second)")
 
 
 
-
-
-
-
-
-
-#stats = pstats.Stats(pr)
-#stats.sort_stats(pstats.SortKey.TIME)
-#now = datetime.datetime.now()
-#profilename = "ProfilingEventHandler"+ now.strftime("D%d_M%m_Y%Y_H%H_M%M_S%S") + ".prof"
-#stats.dump_stats(filename=profilename)
+stats = pstats.Stats(pr)
+stats.sort_stats(pstats.SortKey.TIME)
+now = datetime.datetime.now()
+profilename = "ProfilingEventHandler"+ now.strftime("D%d_M%m_Y%Y_H%H_M%M_S%S") + ".prof"
+stats.dump_stats(filename=profilename)
 
 
 # print("")
