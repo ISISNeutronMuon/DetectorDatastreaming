83dd67038ad4068798f6caad9156df148bb8ccee
Jack-J-Day <jack.day@stfc.ac.uk>
2021-11-05 08:34:14 +0000

    Quick changes
    Commit for testing mantid

--- ReadbackEV42.py
+++ ReadbackEV42.py
@@ -2,7 +2,7 @@ import Send_Kafka_Event
 import datetime
 from streaming_data_types.eventdata_ev42 import deserialise_ev42 # import ESS Flatbuffer serialiser
 
-start_time = (datetime.datetime.now() - datetime.timedelta(minutes=30))
+start_time = (datetime.datetime.now() - datetime.timedelta(minutes=15))
 end_time = datetime.datetime.now()
 print("Getting Data: ")
 KafkaEvents = Send_Kafka_Event.get_data_between(start_time, end_time)
