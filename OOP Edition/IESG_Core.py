7d5adf541f28084eb7c2653e3ba221089ebd03fe
Jack-J-Day <jack.day@stfc.ac.uk>
2021-11-09 08:50:26 +0000

    Update IESG_Core.py
    Added code for:
    - Getting IP from SW pos
    - Getting Port from SW Pos
    - Error Handling

--- OOP Edition/IESG_Core.py
+++ OOP Edition/IESG_Core.py
@@ -87,20 +87,45 @@ class UDPFunctions:
 
 # Define Merlin ADC Processor Board Class - all functions for PC3544m4
 class PC3544:
+
+    #on MADC object creation
     def __init__(self, switchposition):
-        self.switch_pos = int(switchposition)
-        self.control_ipaddress = ""
-        self.control_port = ""
-        self.error_codes = []
-        self.error_desc = []
+        if switchposition in range(32):             # Validate inputted switch position - within 0-31
+            self.switch_pos = int(switchposition)   # If valid get objects switch pos
+        else:                                       # If incorrect add error to error list
+            Error.AddError(17, "Value Range Error - Switch Position is out of range, set to 0", True)
+            self.switch_pos = 0                     # Default to pos 0
 
+        self.MADC_IPs = self.get_network_ip()        # Get IP info from the Switch Position
+        self.MADC_Ports = self.get_network_port()    # Get port info from the Switch Position
+
+        self.control_ipaddress = self.MADC_IPs["BE_FPGA_IP"]    # Get BE/Control IP from dict
+        self.control_port = self.MADC_Ports["BE_FPGA_PORT"]     # Get BE/Control port from dict
+
+    # Function to get the MADC's 5 IP addresses from its switch position
     def get_network_ip(self):
-        pass
+        Possible_BE_IP = [148, 149, 150, 155, 156, 157, 158, 159, 160, 165, 166, 167, 168, 169, 170, 175,
+                          176, 177, 178, 179, 180, 185, 186, 187, 188, 189, 190, 195, 196, 197, 198, 199]
+        BE_FPGA_IP = "192.168.1." + str(Possible_BE_IP[self.switch_pos])        # BE IP = 192.168.1. Matching end number
+        FE_FPGA0_IP = "192.168.2." + str(100+(self.switch_pos * 4))             # Calc FE FPGA0 IP
+        FE_FPGA1_IP = "192.168.2." + str(101+(self.switch_pos * 4))             # Calc FE FPGA1 IP
+        FE_FPGA2_IP = "192.168.2." + str(102+(self.switch_pos * 4))             # Calc FE FPGA2 IP
+        FE_FPGA3_IP = "192.168.2." + str(103+(self.switch_pos * 4))             # Calc FE FPGA3 IP
+        return {"BE_FPGA_IP" : BE_FPGA_IP,"FE_FPGA0_IP" : FE_FPGA0_IP,"FE_FPGA1_IP" : FE_FPGA1_IP,
+                "FE_FPGA2_IP" : FE_FPGA2_IP,"FE_FPGA3_IP" : FE_FPGA3_IP}   # return the Addresses as a dictionary
 
+    # Get the network ports the MADC uses from the switch positon
     def get_network_port(self):
-        pass
+        BE_FPGA_PORT = 0                                # Set BE as port 0 - currently not used
+        FE_FPGA0_PORT = 48640 + (self.switch_pos * 4)   # Calc FE FPGA0 port number
+        FE_FPGA1_PORT = 48641 + (self.switch_pos * 4)   # Calc FE FPGA1 port number
+        FE_FPGA2_PORT = 48642 + (self.switch_pos * 4)   # Calc FE FPGA2 port number
+        FE_FPGA3_PORT = 48643 + (self.switch_pos * 4)   # Calc FE FPGA3 port number
+        return {"BE_FPGA_PORT" : BE_FPGA_PORT, "FE_FPGA0_PORT" : FE_FPGA0_PORT, "FE_FPGA1_PORT" : FE_FPGA1_PORT,
+                "FE_FPGA2_PORT" : FE_FPGA2_PORT, "FE_FPGA3_PORT" : FE_FPGA3_PORT} # Return ports as a dictionary
 
-    def setup_network(self):
+    # Configure a network socket for the control connection to the MADC
+    def setup_control_network(self):
         self.network_socket = UDPFunctions(self.control_ipaddress, self.control_port)
         self.network_socket.open()
 
@@ -140,14 +165,57 @@ class PC3544:
     def set_dest_fpga_ports(self):
         pass
 
+# Error Handler class - define once on program start to be able to log all errors within the program
+class ErrorHandler:
+    def __init__(self):
+        self.ErrorNumberList = []
+        self.ErrorDescList = []
+        print(self.ErrorDescList)
+
+    # Checks to see if the current Error list is valid
+    def CheckErrors_Valid(self):
+        if len(self.ErrorNumberList) != len(self.ErrorDescList):
+            print("Error List Length Mismatch")
+            return False
+
+    def AddError(self, ErrorNumber = 0, ErrorDesc = "unknown error has occured (default)" , printToUser = False):
+        self.ErrorNumberList.append(ErrorNumber)
+        self.ErrorDescList.append(ErrorDesc)
+        self.CheckErrors_Valid()
+        if printToUser:
+            print("An error has occured! - (", ErrorNumber, ")", ErrorDesc)
+
+    # Function to print all errors to terminal, returns false if an errors are invalid, true if printed
+    def PrintAll(self):
+        if self.CheckErrors_Valid() == False:
+            return False
+        if len(self.ErrorNumberList) == 0:
+            print("Program currently has 0 errors")
+            return True
+        else:
+            print("Program currently has ", len(self.ErrorNumberList), " errors")
+        for Error in range(len(self.ErrorNumberList)):
+            print("Error ", Error, ": ", self.ErrorNumberList[Error], " - ", self.ErrorDescList[Error])
+        return True
+
+    # Function to print last errors to terminal, returns false if an errors are invalid, true if printed
+    def PrintLast(self):
+        numErrors = len(self.ErrorNumberList)
+        if self.CheckErrors_Valid() == False:
+            return False
+        if numErrors == 0:
+            print("Program currently has 0 errors")
+            return True
+        else:
+            print("Program currently has ", numErrors, " errors")
+            print("The last error was: ", self.ErrorNumberList[numErrors], " - ", self.ErrorDescList[numErrors])
 
 
+Error = ErrorHandler()
+MADC = []
+if __name__ == "__main__":
+    for SW_POS in range(33):
+        MADC.append(PC3544(SW_POS))
+        print(MADC[SW_POS].get_network_ip())
+        print(MADC[SW_POS].get_network_port())
 
-
-MADC = UDPFunctions("192.168.1.200","1")
-MADC1 = UDPFunctions("192.168.1.201","1")
-
-MADC.open()
-MADC.set_timeout(1)
-MADC.write("FF")
-print(MADC.Port)
